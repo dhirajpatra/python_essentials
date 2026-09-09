eb758dd8b3a6d9467a315ad9f193ee522ee21c1d
Dhiraj Patra <dhiraj.patra@gmail.com>
2026-09-09 15:51:34 +0530

    updated

--- python_coding_interview_prep/python_importan_practice/my_threading.py
+++ python_coding_interview_prep/python_importan_practice/my_threading.py
@@ -1,45 +1,44 @@
-from threading import *
 import threading
 
 
 def new():
     """
-    This is a test method to call threading 
+    This is a call back test method to call threading
     """
     for x in range(6):
-    	print("child executing..", current_thread().getName())
+        print("child executing..", threading.current_thread().name)
 
 
-t1 = Thread(target=new)
+# thread using standard method
+t1 = threading.Thread(target=new)
 t1.start()
 t1.join()
-print("done", current_thread().getName())
+print("done", threading.current_thread().name)
 
 
-# class for threading
+# inheritance from threading class
 class A(threading.Thread):
-	def run(self):
-		for x in range(6):
-			print("child executing..", current_thread().getName())
+    def run(self):
+        for x in range(6):
+            print("child executing..", threading.current_thread().name)
 
 
 objA = A()
 objA.start()
 objA.join()
-print("control returned to main ", current_thread().getName())
+print("control returned to main ", threading.current_thread().name)
 
 
+# using instance method from a custom class
 class ex():
-	def B(self):
-		lst = [5,2,7,'good', 9, 'bad']
-		for x in lst:
-			print("child printing ", x)
+    def B(self):
+        lst = [5, 2, 7, 'good', 9, 'bad']
+        for x in lst:
+            print("child printing ", x)
 
 
 objEx = ex()
-t1 = Thread(target=objEx.B)
+t1 = threading.Thread(target=objEx.B)
 t1.start()
 t1.join()
 print("done")
-
-
